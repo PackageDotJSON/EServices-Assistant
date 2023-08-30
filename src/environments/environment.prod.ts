715e3c8ab75bbf714db92d1f1069458f869cc905
Muhammad Hassan <muhammad.hassan@secp.gov.pk>
2023-08-30 22:59:25 +0500

    disabled ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://srfsctc.secp.gov.pk:3001',
+  baseUrl: 'http://srfsctc.secp.gov.pk:3001',
   dataCleansingKey: 'secpTestAPI@',
   loadingTime: 3000,
 };
